52943970cb787b7270896aee321049ca9e7d192b
suleymankotan <suleymankotan@gmail.com>
2020-07-01 03:33:11 +0300

    Repository added

---
+++ src/main/java/com/example/library_Automation/repository/WriterRepository.java
@@ -0,0 +1,7 @@
+package com.example.library_Automation.repository;
+
+import com.example.library_Automation.entity.WriterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WriterRepository extends JpaRepository<WriterEntity,Long> {
+}
